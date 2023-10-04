5208fb9d84fdf753e48cc3c672f6ef970ac38653
Karina Urazmanova <karina.urazmanova@gmail.com>
2023-10-04 16:04:21 +0300

    Проект по программированию хим задач. День 2

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
+import pandas as pd
+import matplotlib.pyplot as plt
 
-def print_hi(name):
-    print(f'Hi, {name}')
 
-if __name__ == '__main__':
-    print_hi('PyCharm')
+data_cell = pd.read_csv('C:/Users/Karin/PycharmProjects/pythonProject/Datasets/cell_line_descriptors.csv')
+data_cytotoxicity = pd.read_csv('C:/Users/Karin/PycharmProjects/pythonProject/Datasets/Cytotoxicity.csv')
+data_descriptors = pd.read_excel('C:/Users/Karin/PycharmProjects/pythonProject/Datasets/Incomplete_material_descriptors.xlsx')
 
+
+# if __name__ == '__main__':
+#     print('PyCharm')
