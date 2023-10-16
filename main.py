ab7bf6bf9ff3f5d0e178ae32550b100d8dd4344f
Karina Urazmanova <karina.urazmanova@gmail.com>
2023-10-16 23:08:10 +0300

    Build basic ML model

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import pandas as pd
 from Merging_script import merge_script
 from Clean.Cleaning_dataset_script import clean_script
 from Vizualize.vizualize_script import vizualize_script
+from Build.Basic_model import predict_model
 
 data_cell = pd.read_csv('cell_line_descriptors.csv')
 data_cytotoxicity = pd.read_csv('Cytotoxicity.csv')
@@ -13,3 +14,4 @@ if __name__ == '__main__':
     merge_script()
     clean_script()
     vizualize_script()
+    predict_model()
