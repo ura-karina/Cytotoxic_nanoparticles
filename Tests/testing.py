8ced4236c6ca09d25147c1d4a960a9ab2cf50ad4
Karina Urazmanova <karina.urazmanova@gmail.com>
2023-10-16 23:13:40 +0300

    Refactor team work

--- Tests/testing.py
+++ Tests/testing.py
@@ -3,7 +3,7 @@
 import pandas as pd
 import openpyxl
 
-df = pd.read_csv('cytotoxicity_merged.csv')
+df = pd.read_csv('../cytotoxicity_merged.csv')
 
 duplicates = df[df.duplicated()]
 # df.groupby (df.columns.tolist (), as_index= False ). size ()
